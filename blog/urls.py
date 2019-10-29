4b2c2ac770d2679791e9812bd5234dec2d9e6728
hiakki <hy.akshay@gmail.com>
2019-10-30 00:51:52 +0530

    going pro way

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('about/', views.about),
+    path('policy/', views.policy),
+    path('contact/', views.contact),
+    path('tnc/', views.tnc),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
     
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def policy(request):
+    return render(request, 'policy.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def tnc(request):
+    return render(request, 'tnc.html')
