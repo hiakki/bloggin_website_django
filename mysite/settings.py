5b5d9cc5b954103f2d8c395b86766a364a3af911
hyeakki <hye.akshay@gmail.com>
2019-10-29 04:15:36 +0000

    integrated with postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tvdb',
+        'USER': 'chutiyatv',
+        'PASSWORD': 'asdf1234',
+        'HOST': 'postgredb.c8tjsfky7e62.ap-south-1.rds.amazonaws.com',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
